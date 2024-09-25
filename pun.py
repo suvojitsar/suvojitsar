2549579708f147d2f3d14b5a5060bed7fb18f30d
suvojitsar <sarkarsuvojit71@gmail.com>
2024-09-25 16:01:18 +0530

    pun.py

---
+++ pun.py
@@ -0,0 +1,8 @@
+import string
+
+def remove_punctuation(input_string):
+    translator = str.maketrans('', '', string.punctuation)
+    return input_string.translate(translator)
+text = "suvojit@sarkar:section-H:BWU/BTA/22/472:BRAINWARE@UNIVERSITY@#$@%@"
+clean_text = remove_punctuation(text)
+print("String without punctuation:", clean_text)
