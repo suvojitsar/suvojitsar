232952b373b291101e5407ea151254b70157f86d
suvojitsar <sarkarsuvojit71@gmail.com>
2024-09-25 15:58:09 +0530

    py.py

--- py.py
+++ py.py
@@ -1,12 +1,11 @@
 import string
 
 def remove_punctuation(input_string):
-    # Create a translation table for removing punctuation
     translator = str.maketrans('', '', string.punctuation)
     return input_string.translate(translator)
 
-if __name__ == "__main__":
-    # Example usage
-    text = input("Enter a string: ")
-    clean_text = remove_punctuation(text)
-    print("String without punctuation:", clean_text)
+text = "suvojit@sarkar : section-H : bwu/bta/22/472 : BRAINWARE@UNIVERSITY"
+
+clean_text = remove_punctuation(text)
+
+print("String without punctuation:", clean_text)
