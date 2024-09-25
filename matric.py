5c1542a7e172841e48ae72c8eb1a35d90c235556
suvojitsar <sarkarsuvojit71@gmail.com>
2024-09-25 16:13:00 +0530

    matric.py

---
+++ matric.py
@@ -0,0 +1,30 @@
+def add_matrices(matrix1, matrix2):
+    if len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0]):
+        raise ValueError("Matrices must have the same dimensions")
+
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] + matrix2[i][j])
+        result.append(row)
+    
+    return result
+
+matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matrix2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+]
+
+result_matrix = add_matrices(matrix1, matrix2)
+
+print("Resultant Matrix:")
+for row in result_matrix:
+    print(row)
